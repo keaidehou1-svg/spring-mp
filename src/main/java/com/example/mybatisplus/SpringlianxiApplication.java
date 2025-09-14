6f2d71d0c0047a5031dda6c315c4ff42c4913a14
keaidehou1-svg <keaidehou1@gmail.com>
2025-09-14 16:55:01 +0800

    哈哈哈

--- src/main/java/com/example/mybatisplus/SpringlianxiApplication.java
+++ src/main/java/com/example/mybatisplus/SpringlianxiApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//爱卡卡
 public class SpringlianxiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringlianxiApplication.class, args);
     }
-
-
 
 }
